043c56433c338d551d54d6d83c4426f384b4db24
Lee Kadin <lkadin@gmail.com>
2023-12-30 11:15:03 -0500

    Handle web socket from client or html

---
+++ connection_manager.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import json
+
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+class ConnectionManager:
+    def __init__(self) -> None:
+        self.active_connections: list[str, WebSocket] = {}
+        print("Creating a list to hold active connections", self.active_connections)
+
+    async def connect(self, room_id: str, websocket: WebSocket):
+        await websocket.accept()
+        if not self.active_connections.get(room_id):
+            self.active_connections[room_id] = []
+        self.active_connections[room_id].append(websocket)
+        print("New Active connections are ", self.active_connections)
+
+    async def disconnect(self, room_id: str, websocket: WebSocket):
+        self.active_connections[room_id].remove(websocket)
+        print("After disconnect active connections are: ", self.active_connections)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+        print("Sent a personal msg to , ", websocket)
+
+    async def broadcast(self, message: str, room_id: str, websocket: WebSocket):
+        for connection in self.active_connections[room_id]:
+            print(connection.client)
+            print(connection.url)
+            await connection.send_text(message)
+            print(f"In broadcast: sent {message} to ", room_id, connection.client)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{room_id}")
+async def websocket_chat(websocket: WebSocket, room_id: str):
+    print('you are here')
+    await manager.connect(room_id, websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            message = json.loads(data)
+            print(message)
+            await manager.broadcast(
+                f" {message['user_name']} says: {message['message_txt']}",
+                room_id,
+                websocket,
+            )
+    except Exception as e:
+        print("Got an exception ", e)
+        await manager.disconnect(room_id, websocket)
