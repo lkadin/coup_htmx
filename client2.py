0cfad8c85237e8f7a1a46ce8cb34385d2a7dbad8
Lee Kadin <lkadin@gmail.com>
2023-12-31 17:25:52 -0500

    Handle multiple messages

--- client2.py
+++ client2.py
@@ -2,18 +2,21 @@ import asyncio
 import websockets
 import json
 
+USER_NAME='Client 2'
 
 async def test():
-    message={"user_name":"Fred","message_txt":"This is my message"}
+    message={"user_name":USER_NAME,"message_txt":"This is my message"}
     json_message=json.dumps(message)
     async with websockets.connect(
         "ws://localhost:8000/ws/1"
     ) as websocket:
-        await websocket.send(json_message)
-        response = await websocket.recv()
-        print(response)
-        response = await websocket.recv()
-        print(response)
+        while True:
+            message=input ("enter Message   ")
+            new_message={"user_name":USER_NAME,"message_txt":message}
+            json_message=json.dumps(new_message)
+            await websocket.send(json_message)
+            response = await websocket.recv()
+            print(response)
 
 
 asyncio.get_event_loop().run_until_complete(test())
