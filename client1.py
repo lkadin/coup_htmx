e24ef63c55e34b0beed1c1373c6e8dc0590ecebe
Lee Kadin <lkadin@gmail.com>
2024-01-01 14:09:53 -0500

    Black

--- client1.py
+++ client1.py
@@ -2,19 +2,20 @@ import asyncio
 import websockets
 import json
 
+USER_NAME = "Client 1"
+
 
 async def test():
-    message={"user_name":"Lee","message_txt":"This is my message"}
-    json_message=json.dumps(message)
-    print(f"string - {json_message}")
-    async with websockets.connect(
-        "ws://localhost:8000/ws/1"
-    ) as websocket:
-        await websocket.send(json_message)
-        response = await websocket.recv()
-        print(response)
-        response = await websocket.recv()
-        print(response)
+    message = {"user_name": USER_NAME, "message_txt": "This is my message"}
+    json_message = json.dumps(message)
+    async with websockets.connect("ws://localhost:8000/ws/1") as websocket:
+        while True:
+            message = input("enter Message   ")
+            new_message = {"user_name": USER_NAME, "message_txt": message}
+            json_message = json.dumps(new_message)
+            await websocket.send(json_message)
+            response = await websocket.recv()
+            print(response)
 
 
 asyncio.get_event_loop().run_until_complete(test())
--- client2.py
+++ client2.py
@@ -2,18 +2,17 @@ import asyncio
 import websockets
 import json
 
-USER_NAME='Client 2'
+USER_NAME = "Client 2"
+
 
 async def test():
-    message={"user_name":USER_NAME,"message_txt":"This is my message"}
-    json_message=json.dumps(message)
-    async with websockets.connect(
-        "ws://localhost:8000/ws/1"
-    ) as websocket:
+    message = {"user_name": USER_NAME, "message_txt": "This is my message"}
+    json_message = json.dumps(message)
+    async with websockets.connect("ws://localhost:8000/ws/1") as websocket:
         while True:
-            message=input ("enter Message   ")
-            new_message={"user_name":USER_NAME,"message_txt":message}
-            json_message=json.dumps(new_message)
+            message = input("enter Message   ")
+            new_message = {"user_name": USER_NAME, "message_txt": message}
+            json_message = json.dumps(new_message)
             await websocket.send(json_message)
             response = await websocket.recv()
             print(response)
