ee4e866e4ffe59fa68a13aebf0eaa6d4eed8e88a
Lee Kadin <lkadin@gmail.com>
2024-02-11 09:02:13 -0500

    Move card stuff to coup.py

--- connection_manager.py
+++ connection_manager.py
@@ -5,7 +5,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 import uvicorn
 from datetime import datetime
-import random
 
 app = FastAPI()
 
@@ -15,26 +14,6 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-class Card:
-    def __init__(self, value) -> None:
-        self, value = value
-
-
-class Deck:
-    def __init__(self) -> None:
-        self.cards = []
-        for value in ["Duke", "Assassin", "Ambassador", "Captain", "Contessa"]:
-            for _ in range(3):
-                self.cards.append(value)
-
-    def shuffle(self):
-        random.shuffle(self.cards)
-
-    def draw(self):
-        return self.cards.pop()
-
-    def __repr__(self) -> str:
-        return ' '.join([self.card for self.card in self.cards])
 
 class ConnectionManager:
     def __init__(self) -> None:
@@ -80,10 +59,6 @@ class ConnectionManager:
         await self.send_personal_message(content, self.active_connections["3"][0])
 
 
-deck = Deck()
-deck.shuffle()
-print(deck.draw())
-print(deck)
 manager = ConnectionManager()
 
 
