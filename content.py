087225ba3cf5f39587bcb3b1be28e39522d0879c
Lee Kadin <lkadin@gmail.com>
2024-02-13 15:30:03 -0500

    refactor to make generic content

--- content.py
+++ content.py
@@ -3,46 +3,7 @@ class Content:
         self.player_ids = player_ids
         self.user_id = user_id
 
-    def content1(self):
-        return """
-            <div hx-swap-oob="innerHTML:#photo">
-            <img src="/static/jpg/duke.JPG" alt="duke">
-            </div>
-            """
-
-    def content2(self):
-        self.table = """
-                <div hx-swap-oob="innerHTML:#photo">
-                <table style='border-collapse: collapse;'>
-                <col width="40%">
-                <col width="40%">
-                <tr>
-                    <td>
-                        <img src="/static/jpg/down.png">
-                    </td>
-                    <td>
-                        <img src="/static/jpg/down.png">
-                    </td>
-                </tr>
-                <tr>
-                    <td><br><br></td>
-                </tr>
-                <tr>
-                    
-                </tr>
-                </table>
-                </div>
-            """
-        return self.table
-
-    def content3(self):
-        return """
-            <div hx-swap-oob="innerHTML:#photo">
-            <img src="/static/jpg/duke.JPG" alt="duke">
-            </div>
-            """
-
-    def content9(self):
+    def content_generic(self, user_id):
         self.table = """
             <div hx-swap-oob="innerHTML:#photo">
             <table style='border-collapse: collapse;'>
@@ -75,12 +36,4 @@ class Content:
         return self.table
 
     def html(self):
-        if self.user_id == "1":
-            return self.content1()
-        if self.user_id == "2":
-            return self.content2()
-        if self.user_id == "3":
-            return self.content3()
-        if self.user_id == "9":
-            return self.content9()
-        return self.content9
+        return self.content_generic(self)
