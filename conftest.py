14108d8efdb61049e9cfc69f037eb53904d567ff
Lee Kadin <lkadin@gmail.com>
2024-02-29 12:26:29 -0500

    added content and game_ready

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from coup import Card
 from coup import Deck
 from coup import Player
 from coup import Game
+from content import Content
 
 
 @pytest.fixture
@@ -23,3 +24,17 @@ def deck():
 @pytest.fixture
 def game():
     return Game()
+
+
+@pytest.fixture
+def game_ready():
+    ids = [("1", "Lee"), ("2", "Adina"), ("3", "Joey"), ("9", "Jamie")]
+    game = Game()
+    game.add_all_players(ids)
+    game.start()
+    return game
+
+
+@pytest.fixture
+def content():
+    return Content(game_ready(), "1")
--- tests/test_content.py
+++ tests/test_content.py
@@ -1,11 +1,6 @@
-from coup import Game
-from content import Content
-
-
-def test_show_hand(deck):
+def test_show_hand(deck, content, game):
     ids = [("1", "Lee"), ("2", "Adina"), ("3", "Joey"), ("9", "Jamie")]
-    game = Game()
     game.add_all_players(ids)
     game.start()
-    content = Content(game, "1")
+    # content = Content(game, "1")
     assert len(content.show_table()) >= 100
