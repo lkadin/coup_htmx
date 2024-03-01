d9c22901acdbd81e3c76d4d78ed0875876f5e37e
Lee Kadin <lkadin@gmail.com>
2024-03-01 11:09:08 -0500

    add new tests

--- tests/test_content.py
+++ tests/test_content.py
@@ -1,4 +1,11 @@
 def test_show_hand(content, game_ready):
-    # content = Content(game_ready, "1")
     player = game_ready.player("1")
     assert len(content.show_hand(player)) >= 100
+
+
+def test_show_table(content):
+    assert len(content.show_table()) >= 100
+
+
+def test_show_actions(content):
+    assert len(content.show_actions()) >= 100
