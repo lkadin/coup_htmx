fad46ccc26b66f8bb8c4cdbba2240db0c43275e2
Lee Kadin <lkadin@gmail.com>
2024-03-01 11:03:39 -0500

    moved content and game_ready to fixtures

--- conftest.py
+++ conftest.py
@@ -27,14 +27,13 @@ def game():
 
 
 @pytest.fixture
-def game_ready():
+def game_ready(game):
     ids = [("1", "Lee"), ("2", "Adina"), ("3", "Joey"), ("9", "Jamie")]
-    game = Game()
     game.add_all_players(ids)
     game.start()
     return game
 
 
 @pytest.fixture
-def content():
-    return Content(game_ready(), "1")
+def content(game_ready):
+    return Content(game_ready, "1")
--- tests/test_content.py
+++ tests/test_content.py
@@ -1,6 +1,4 @@
-def test_show_hand(deck, content, game):
-    ids = [("1", "Lee"), ("2", "Adina"), ("3", "Joey"), ("9", "Jamie")]
-    game.add_all_players(ids)
-    game.start()
-    # content = Content(game, "1")
-    assert len(content.show_table()) >= 100
+def test_show_hand(content, game_ready):
+    # content = Content(game_ready, "1")
+    player = game_ready.player("1")
+    assert len(content.show_hand(player)) >= 100
