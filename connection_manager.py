6cf52359391ad15bf4b9a20700b0261d2aebdf91
Lee Kadin <lkadin@gmail.com>
2024-03-18 10:58:18 -0400

    Hide second player selection after picking

--- connection_manager.py
+++ connection_manager.py
@@ -49,3 +49,9 @@ class ConnectionManager:
                 await self.send_personal_message(
                     table, self.active_connections[user_id]
                 )
+
+            if message_type in ("hide") and self.game.your_turn(user_id):
+                table = content.hide_second_player()
+                await self.send_personal_message(
+                    table, self.active_connections[user_id]
+                )
