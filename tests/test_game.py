698379dc34994e4b0264455144ccb12723f929f5
Lee Kadin <lkadin@gmail.com>
2024-03-19 13:16:59 -0400

    use game_ready for tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -22,22 +22,20 @@ class TestGame:
         game.next_turn()
         assert game.whose_turn() == 1
 
-    def test_whose_turn_name(self, game, ids):
-        game.add_all_players(ids)
-        assert game.whose_turn_name() == game.player_ids[game.current_player_index][1]
+    def test_whose_turn_name(self, game_ready, ids):
+        assert (
+            game_ready.whose_turn_name()
+            == game_ready.player_ids[game_ready.current_player_index][1]
+        )
 
-    def test_whose_turn(self, game, ids):
-        game.add_all_players(ids)
-        assert isinstance(game.whose_turn(), int)
+    def test_whose_turn(self, game_ready, ids):
+        assert isinstance(game_ready.whose_turn(), int)
 
-    def test_add_all_actions(self, game):
-        game.add_all_actions()
-        assert len(game.actions) == 9
+    def test_add_all_actions(self, game_ready):
+        assert len(game_ready.actions) == 9
 
-    def test_enable_all_actions(self, game):
-        game.add_all_actions()
-        game.enable_all_actions()
-        for action in game.actions:
+    def test_enable_all_actions(self, game_ready):
+        for action in game_ready.actions:
             assert action.status == "enabled"
 
     def test_wait(self, game):
@@ -49,47 +47,37 @@ class TestGame:
         assert game.deck is not None
         assert len(game.actions) > 0
 
-    def test_your_turn(self, game, ids):
+    def test_your_turn(self, game_ready, ids):
         user_id = "1"
-        game.start()
-        game.add_all_players(ids)
-        assert isinstance(game.your_turn(user_id), bool)
+        assert isinstance(game_ready.your_turn(user_id), bool)
 
-    def test_player(self, game, ids):
-        game.add_all_players(ids)
-        game.start()
-        assert isinstance(game.player("1"), Player)
+    def test_player(self, game_ready, ids):
+        assert isinstance(game_ready.player("1"), Player)
 
-    def test_initial_deal(self, game, ids):
-        game.add_all_players(ids)
-        game.start()
-        for player in game.players.values():
+    def test_initial_deal(self, game_ready, ids):
+        for player in game_ready.players.values():
             assert len(player.hand) == 2
 
-    def test_action_from_action_name(self, game):
-        game.add_all_actions()
-        for action in game.actions:
-            assert isinstance(game.action_from_action_name(action.name), Action)
-        assert game.action_from_action_name(None) is None
-        assert game.action_from_action_name("FRED") is None
-        assert isinstance(game.action_from_action_name("Assassinate"), Action)
+    def test_action_from_action_name(self, game_ready):
+        for action in game_ready.actions:
+            assert isinstance(game_ready.action_from_action_name(action.name), Action)
+        assert game_ready.action_from_action_name(None) is None
+        assert game_ready.action_from_action_name("FRED") is None
+        assert isinstance(game_ready.action_from_action_name("Assassinate"), Action)
 
-    def test_process_actions(self, game, ids):
-        game.add_all_players(ids)
-        game.add_all_actions()
+    def test_process_actions(self, game_ready, ids):
         user_id = "1"
-        for action in game.actions:
-            assert game.process_action(action, user_id) is None
+        for action in game_ready.actions:
+            assert game_ready.process_action(action, user_id) is None
 
-    def test_steal(self, game, ids):
-        game.add_all_players(ids)
-        game.start()
-        coins1 = game.players["1"].coins
-        coins2 = game.players["2"].coins
-        game.steal(give_to=game.players["1"].id, steal_from=game.players["2"].id)
-        assert game.players["1"].coins == coins1 + 2
-        assert game.players["2"].coins == coins2 - 2
+    def test_steal(self, game_ready, ids):
+        coins1 = game_ready.players["1"].coins
+        coins2 = game_ready.players["2"].coins
+        game_ready.steal(
+            give_to=game_ready.players["1"].id, steal_from=game_ready.players["2"].id
+        )
+        assert game_ready.players["1"].coins == coins1 + 2
+        assert game_ready.players["2"].coins == coins2 - 2
 
-    def test_player_id(self, game, ids):
-        game.add_all_players(ids)
-        assert game.player_id("Lee") == "1"
+    def test_player_id(self, game_ready, ids):
+        assert game_ready.player_id("Lee") == "1"
