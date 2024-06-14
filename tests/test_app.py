e38ecddd94879835c2c5cdc81131039c98ad20b1
Lee Kadin <lkadin@gmail.com>
2024-06-14 17:09:32 -0400

    Additional tests for test_read_item

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,11 +19,23 @@ def test_read_item(client):
     response = client.get("/web/1/?user_name=Lee")
     assert response.status_code == 200
     assert "Lee" in response.text
+    assert "Assassinate" in response.text
+    assert "Coup" in response.text
+    assert "Steal" in response.text
+    assert "Take_3_coins" in response.text
+    assert "Foreign_aid" in response.text
+    assert "Income" in response.text
+    assert "Exchange" in response.text
+    assert "Block" in response.text
+    assert "Challenge" in response.text
+    assert "Accept_Block" in response.text
+    assert "Start" in response.text
+    assert "second_player" in response.text
 
 
 def test_websocket_connect(client):
     with client.websocket_connect("/ws/1") as websocket:
-        data = {"message_txt": "Test message"}
+        data = {"message_txt": "Start"}
         websocket.send_text(json.dumps(data))
         response = websocket.receive_text()
         assert "alert" in response
